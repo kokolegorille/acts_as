9bd63b3028d064c4c9520759cd1f4a0a0030e86b
kokolegorille <koko.le.gorille@gmail.com>
2019-12-16 03:56:43 +0100

    Add tests for acts as list

--- test/list/list_test.exs
+++ test/list/list_test.exs
@@ -30,6 +30,28 @@ defmodule ActsAs.ListTest do
       {:ok, dummy} = DummyListContext.create_dummy(%{}, parent)
       assert dummy.position == 1
     end
+
+    test "creates multiple lists with metadata", context do
+      parent = context[:parent]
+      {:ok, dummy} = DummyListContext.create_dummy(%{}, parent)
+      {:ok, dummy2} = DummyListContext.create_dummy(%{}, parent)
+      assert dummy.position == 1
+      assert dummy2.position == 2
+    end
+
+    test "delete update list metadata", context do
+      parent = context[:parent]
+      {:ok, dummy} = DummyListContext.create_dummy(%{}, parent)
+      {:ok, dummy2} = DummyListContext.create_dummy(%{}, parent)
+      {:ok, dummy3} = DummyListContext.create_dummy(%{}, parent)
+      assert dummy.position == 1
+      assert dummy2.position == 2
+      assert dummy3.position == 3
+
+      DummyListContext.delete_dummy(dummy2)
+      dummy3 = DummyListContext.get_dummy(dummy3.id)
+      assert dummy3.position == 2
+    end
   end
 
 end
